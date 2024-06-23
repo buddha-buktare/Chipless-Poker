6cd4b78db4dc049bb16352b8d8791e34c39a956d
buddha-buktare <buddha.buktare@gmail.com>
2024-06-23 18:25:32 +0530

    UseCases usage in viewModel

--- app/src/main/kotlin/me/buddha/chiplesspoker/ui/GameViewModel.kt
+++ app/src/main/kotlin/me/buddha/chiplesspoker/ui/GameViewModel.kt
@@ -3,6 +3,7 @@ package me.buddha.chiplesspoker.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import me.buddha.chiplesspoker.domain.StreetType.PREFLOP
 import me.buddha.chiplesspoker.domain.model.BlindLevel
@@ -18,6 +19,35 @@ import me.buddha.chiplesspoker.domain.usecase.InsertOrReplaceGameUseCase
 import java.time.LocalDateTime
 import javax.inject.Inject
 
+val SAMPLE_GAME = Game(
+    id = 4,
+    dateSaved = LocalDateTime.now(),
+    initialBuyIn = 1000,
+    street = PREFLOP,
+    pots = listOf(
+        Pot(50000, listOf())
+    ),
+    blindStructure = BlindStructure(
+        durationUnit = "Minutes",
+        levels = listOf(
+            BlindLevel(
+                level = 1,
+                big = 6,
+                small = 3,
+                duration = 30
+            )
+        )
+    ),
+    players = listOf(
+        Player(
+            name = "Buddha",
+            seatNumber = 1,
+            chips = 199,
+            isDealer = true
+        )
+    )
+)
+
 @HiltViewModel
 class GameViewModel @Inject constructor(
     private val deleteAllUseCase: DeleteAllUseCase,
@@ -29,36 +59,43 @@ class GameViewModel @Inject constructor(
 
     init {
         viewModelScope.launch {
-            insertOrReplaceGameUseCase(
-                Game(
-                    id = 4,
-                    dateSaved = LocalDateTime.now(),
-                    initialBuyIn = 1000,
-                    street = PREFLOP,
-                    pots = listOf(
-                        Pot(50000, listOf())
-                    ),
-                    blindStructure = BlindStructure(
-                        durationUnit = "Minutes",
-                        levels = listOf(
-                            BlindLevel(
-                                level = 1,
-                                big = 6,
-                                small = 3,
-                                duration = 30
-                            )
-                        )
-                    ),
-                    players = listOf(
-                        Player(
-                            name = "Buddha",
-                            seatNumber = 1,
-                            chips = 199,
-                            isDealer = true
-                        )
-                    )
-                )
-            )
+            insertGame(SAMPLE_GAME)
+            delay(10000)
+            deleteAllGames()
+        }
+    }
+
+    private fun deleteAllGames() {
+        viewModelScope.launch {
+            deleteAllUseCase()
+        }
+    }
+
+    private fun deleteGame(id: Long) {
+        viewModelScope.launch {
+            deleteByIdUseCase(id)
+        }
+    }
+
+    private fun getAllGames() {
+        viewModelScope.launch {
+            getAllGamesUseCase().collect {
+
+            }
+        }
+    }
+
+    private fun getGameDetails(id: Long) {
+        viewModelScope.launch {
+            getGameByIdUseCase(id).collect {
+
+            }
+        }
+    }
+
+    private fun insertGame(game: Game) {
+        viewModelScope.launch {
+            insertOrReplaceGameUseCase(game)
         }
     }
 }
