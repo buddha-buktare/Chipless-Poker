29583503a4ebcb52a01920edfc75efaf8f119012
buddha-buktare <buddha.buktare@gmail.com>
2025-02-11 19:32:06 +0530

    Added firebase.

--- app/src/main/kotlin/me/buddha/chiplesspoker/domain/ChiplessPokerApp.kt
+++ app/src/main/kotlin/me/buddha/chiplesspoker/domain/ChiplessPokerApp.kt
@@ -1,7 +1,13 @@
 package me.buddha.chiplesspoker.domain
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ChiplessPokerApp : Application()
+class ChiplessPokerApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
+}
